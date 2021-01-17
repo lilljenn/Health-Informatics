5b88d9f1f1ac7914e965c4b77a4a5744036872f7
lilljenn <64298940+lilljenn@users.noreply.github.com>
2021-01-17 15:30:08 -0600

    Update genes.js

--- genes.js
+++ genes.js
@@ -7,12 +7,11 @@
             });
         }
 */
- function goPython(){
-            $.ajax({
-              url: "genes.py",
-             context: document.body
-            }).done(function() {
-             alert('finished python script');;
-            });
-        }
+$(document).ready(function(){
+    $('#getdata-button').live('click', function(){
+        $.getJSON('/json-data', {'nitems': 3}, function(data) {
+            $('#showdata').html("<p>item1="+data.item1+" item2="+data.item2+" item3="+data.item3+"</p>");
+        });
+    });
+}); 
 
