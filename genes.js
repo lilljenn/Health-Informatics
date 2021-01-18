8acf6654f913e70ba92cfd0c8208a95273b13584
lilljenn <64298940+lilljenn@users.noreply.github.com>
2021-01-17 22:54:09 -0600

    Update genes.js

--- genes.js
+++ genes.js
@@ -1,16 +1,17 @@
-      /* function goPython(){
+      function goPython(){
             $.ajax({
-              url: "genes.py",
+              url: "lilljenn.github.io/Health-Informatics/genes.py",
              context: document.body
             }).done(function() {
              alert('finished python script');;
             });
         }
-*/
+
+/*
 function goPython() {
   document.getElementById("genes").innerHTML = "inner html";
-  document.getElementById("genes").href = "lilljenn.github.io/HealthInformatics/genes.py";
+  document.getElementById("genes").href = "lilljenn.github.io/Health-Informatics/genes.py";
   document.getElementById("genes").target = "_blank";
 }
 
-
+*/
