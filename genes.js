d0e53e9add4fcb883f2487824ab5770bae427346
lilljenn <64298940+lilljenn@users.noreply.github.com>
2021-01-21 12:16:22 -0600

    Update genes.js

--- genes.js
+++ genes.js
@@ -1,15 +1,5 @@
 //function goPython() {
-function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "lilljenn.github.io/Health-Informatics/genes.py", true);
-  xhttp.send();
-}
+print("text");
 //}
 /*
 
