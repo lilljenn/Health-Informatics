66b7f2601e49216a8316c8883f9cdfddc6a5368f
lilljenn <64298940+lilljenn@users.noreply.github.com>
2021-01-22 18:03:25 -0600

    Delete resize.js

--- resize.js
+++
@@ -1,8 +0,0 @@
-function showLinks() {
-var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
